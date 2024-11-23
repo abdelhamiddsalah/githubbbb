bd74a26b039feb63876f869ad1a94587ae4f0eca
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-11-23 13:29:05 +0200

    newww

---
+++ lib/newfile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Newfile extends StatelessWidget {
+  const Newfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
