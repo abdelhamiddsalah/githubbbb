a214f60cb88f5c70dfeffaa6fde22ea0bb988ae3
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-11-23 13:34:34 +0200

    new hamid

---
+++ lib/hamid.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Hamid extends StatelessWidget {
+  const Hamid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
